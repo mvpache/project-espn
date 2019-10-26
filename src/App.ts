c0d491ba4735e51ecf5b4d4d5e563f1b3760ce80
mvpache <admin@ITLAPTOP.TBOX.local>
2019-10-26 12:24:46 -0700

    meta files

--- src/App.ts
+++ src/App.ts
@@ -3,20 +3,20 @@ import * as express from 'express';
 class App {
   public express
 
-  constructor () {
+  constructor() {
     this.express = express();
     this.mountRoutes();
   }
 
-  private mountRoutes (): void {
+  private mountRoutes(): void {
     const router = express.Router();
     router.get('/', (req, res) => {
       res.json({
-        message: 'Hello World!'
+        message: 'Hello World!',
       });
     });
     this.express.use('/', router);
   }
-};
+}
 
 export default new App().express;
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import app from './App';
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, (err) => {
+app.listen(port, err => {
   if (err) {
     return console.log(err);
-  };
+  }
 
-  return console.log(`server is listening on ${port}`)
+  return console.log(`server is listening on ${port}`);
 });
